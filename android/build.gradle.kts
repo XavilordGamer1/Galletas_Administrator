08f5abee796c2e3c1140ab052889f43fcca1c81a
XavilordGamer1 <xavier.18.2018@hotmail.com>
2025-07-07 21:35:13 -0500

    guardo las versiones que tengo es una prueba

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,37 +1,21 @@
-    // --- BLOQUE AÑADIDO PARA DEFINIR LA VERSIÓN CORRECTA ---
-    buildscript {
-        repositories {
-            google()
-            mavenCentral()
-        }
-        dependencies {
-            // Esta es la versión compatible que solucionará el error
-            classpath "com.android.tools.build:gradle:7.3.1"
-            // La versión de Kotlin que usa Flutter
-            classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22"
-        }
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
-    // --- FIN DEL BLOQUE AÑADIDO ---
+}
 
-    allprojects {
-        repositories {
-            google()
-            mavenCentral()
-        }
-    }
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
 
-    val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-    rootProject.layout.buildDirectory.value(newBuildDir)
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
 
-    subprojects {
-        val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-        project.layout.buildDirectory.value(newSubprojectBuildDir)
-    }
-    subprojects {
-        project.evaluationDependsOn(":app")
-    }
-
-    tasks.register<Delete>("clean") {
-        delete(rootProject.layout.buildDirectory)
-    }
-    
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
